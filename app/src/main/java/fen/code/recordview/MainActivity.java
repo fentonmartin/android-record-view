b8a3edb5c6942950b1aebeb7e0f6fbca40d91f37
Fenton Martin <fentonxm@gmail.com>
2018-10-09 00:16:06 +0700

    Set record listener on record view

--- app/src/main/java/fen/code/recordview/MainActivity.java
+++ app/src/main/java/fen/code/recordview/MainActivity.java
@@ -1,9 +1,11 @@
 package fen.code.recordview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.devlomi.record_view.OnRecordListener;
 import com.devlomi.record_view.RecordButton;
 import com.devlomi.record_view.RecordView;
 
@@ -25,5 +27,33 @@ public class MainActivity extends AppCompatActivity {
 
         /* set record button on view */
         recordButton.setRecordView(recordView);
+
+        /* set record listener on record view */
+        recordView.setOnRecordListener(new OnRecordListener() {
+            @Override
+            public void onStart() {
+                // On Start Recording
+                Log.d("RecordView", "onStart");
+            }
+
+            @Override
+            public void onCancel() {
+                // On Swipe To Cancel
+                Log.d("RecordView", "onCancel");
+
+            }
+
+            @Override
+            public void onFinish(long recordTime) {
+                // On Stop Recording
+                Log.d("RecordView", "onFinish");
+            }
+
+            @Override
+            public void onLessThanSecond() {
+                // When the record time is less than One Second
+                Log.d("RecordView", "onLessThanSecond");
+            }
+        });
     }
 }
